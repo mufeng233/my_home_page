7c0a8bfb9d97a0c8ed5afa3584dd9226c30da35d
mufeng2333 <2441260435@qq.com>
2022-05-05 20:13:43 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="navbar bg-base-100 shadow-lg">
+    <!-- <div class="navbar bg-base-100 shadow-lg">
       <div class="flex-none">
         <label class="btn btn-square btn-ghost drawer-button" for="my-drawer">
           <svg class="inline-block w-7 h-7" viewBox="0 0 24 24">
@@ -33,7 +33,7 @@
           </ul>
         </div>
       </div>
-    </div>
+    </div> -->
     <div class="drawer">
       <input id="my-drawer" type="checkbox" class="drawer-toggle" />
       <div class="drawer-content">
@@ -44,24 +44,29 @@
           <div class="hero-overlay bg-opacity-60"></div>
           <div class="hero-content text-center text-neutral-content">
             <div class="max-w-md">
-              <h1 class="mb-5 text-5xl font-bold">👋你好，我是沐风</h1>
-              <p class="mb-5">
+              <h1 class="mb-5 text-2xl md:text-5xl font-bold">
+                👋你好，我是沐风
+              </h1>
+              <p class="mb-5 text-sm md:text-lg">
                 我始终相信，走过平湖烟雨 岁月山河<br />
                 那些历经劫数，尝遍百味的人<br />
                 会更加生动而干净。
               </p>
-              <a
-                class="btn btn-ghost btn-circle"
-                href="http://wpa.qq.com/msgrd?v=3&uin=2441260435&site=qq&menu=yes"
-                target="_blank"
-              >
-                <svg class="w-8 h-8" viewBox="0 0 24 24">
-                  <path
-                    fill="currentColor"
-                    d="M3.18,13.54C3.76,12.16 4.57,11.14 5.17,10.92C5.16,10.12 5.31,9.62 5.56,9.22C5.56,9.19 5.5,8.86 5.72,8.45C5.87,4.85 8.21,2 12,2C15.79,2 18.13,4.85 18.28,8.45C18.5,8.86 18.44,9.19 18.44,9.22C18.69,9.62 18.84,10.12 18.83,10.92C19.43,11.14 20.24,12.16 20.82,13.55C21.57,15.31 21.69,17 21.09,17.3C20.68,17.5 20.03,17 19.42,16.12C19.18,17.1 18.58,18 17.73,18.71C18.63,19.04 19.21,19.58 19.21,20.19C19.21,21.19 17.63,22 15.69,22C13.93,22 12.5,21.34 12.21,20.5H11.79C11.5,21.34 10.07,22 8.31,22C6.37,22 4.79,21.19 4.79,20.19C4.79,19.58 5.37,19.04 6.27,18.71C5.42,18 4.82,17.1 4.58,16.12C3.97,17 3.32,17.5 2.91,17.3C2.31,17 2.43,15.31 3.18,13.54Z"
-                  />
-                </svg>
-              </a>
+              <div class="tooltip" data-tip="2441260435">
+                <a
+                  class="btn btn-ghost btn-circle"
+                  href="http://wpa.qq.com/msgrd?v=3&uin=2441260435&site=qq&menu=yes"
+                  target="_blank"
+                >
+                  <svg class="w-8 h-8" viewBox="0 0 24 24">
+                    <path
+                      fill="currentColor"
+                      d="M3.18,13.54C3.76,12.16 4.57,11.14 5.17,10.92C5.16,10.12 5.31,9.62 5.56,9.22C5.56,9.19 5.5,8.86 5.72,8.45C5.87,4.85 8.21,2 12,2C15.79,2 18.13,4.85 18.28,8.45C18.5,8.86 18.44,9.19 18.44,9.22C18.69,9.62 18.84,10.12 18.83,10.92C19.43,11.14 20.24,12.16 20.82,13.55C21.57,15.31 21.69,17 21.09,17.3C20.68,17.5 20.03,17 19.42,16.12C19.18,17.1 18.58,18 17.73,18.71C18.63,19.04 19.21,19.58 19.21,20.19C19.21,21.19 17.63,22 15.69,22C13.93,22 12.5,21.34 12.21,20.5H11.79C11.5,21.34 10.07,22 8.31,22C6.37,22 4.79,21.19 4.79,20.19C4.79,19.58 5.37,19.04 6.27,18.71C5.42,18 4.82,17.1 4.58,16.12C3.97,17 3.32,17.5 2.91,17.3C2.31,17 2.43,15.31 3.18,13.54Z"
+                    />
+                  </svg>
+                </a>
+              </div>
+
               <a
                 class="btn btn-ghost btn-circle"
                 href="https://github.com/mufeng233"
